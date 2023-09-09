c2164cc9ac52c8c4336f8eba6d87090a3fd0f027
hachi868 <24986912+hachi868@users.noreply.github.com>
2023-09-10 01:24:54 +0900

    :sparkles:[add]ex00 base

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -1,5 +1,31 @@
-//
-// Created by hco on 2023/09/08.
-//
+
 
 #include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(std::string	name)
+{
+	name = name;
+	hit_points = 10;
+	energy_points = 10;
+	attack_damage = 0;
+}
+
+ClapTrap::~ClapTrap()
+{
+
+}
+
+void ClapTrap::attack(const std::string& target)
+{
+
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+
+}
--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -1,13 +1,21 @@
-//
-// Created by hco on 2023/09/08.
-//
 
 #ifndef __CLAPTRAP_H__
 #define __CLAPTRAP_H__
 
 
 class ClapTrap {
+public:
+	ClapTrap(std::string name);
+	~ClapTrap();
+	void attack(const std::string& target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
 
+private:
+	std::string	name;
+	unsigned int	hit_points;
+	unsigned int	energy_points;
+	unsigned int	attack_damage;
 };
 
 
