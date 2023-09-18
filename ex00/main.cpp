7a1685b85daa4e1f862e804c88396d63de5120f9
hachi868 <24986912+hachi868@users.noreply.github.com>
2023-09-18 16:52:51 +0900

    :dizzy:[update]ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -7,49 +7,52 @@ __attribute__((destructor)) static void destructor()
 }
 
 void testLoop(ClapTrap &ct) {
-	(void)ct;
-//	int i = 0;
-//	std::string target = "target";
-//
-//	std::cout << "inited value:" << std::endl;
-//	ct.showInfo();
-//	std::cout << std::endl;
-//	while (i < 10)
-//	{
-//		if (i == 2 || i == 5 || i == 8)
-//			ct.takeDamage(5);
-//		else if (i == 3 || i == 7)
-//			ct.beRepaired(5);
-//		else
-//			ct.attack(target);
-//		ct.showInfo();
-//		i++;
-//	}
+	int i = 0;
+	std::string target = "target";
+
+	std::cout << "inited value: ";
+	ct.showInfo();
+	std::cout << std::endl;
+	while (i < 10)
+	{
+		if (i == 2 || i == 5 || i == 8)
+			ct.takeDamage(5);
+		else if (i == 3 || i == 7)
+			ct.beRepaired(5);
+		else
+			ct.attack(target);
+		ct.showInfo();
+		i++;
+	}
 	std::cout << "-----" << std::endl;
 }
 
-int main( void ) {
-	ClapTrap ct00;
-	testLoop(ct00);
-	std::cout << "[test0] name constructor" << std::endl;
-	ClapTrap ct0("name_ct0");
-	testLoop(ct0);
+int main( void )
+{
+	std::cout << "[test0] Default constructor" << std::endl;
+	ClapTrap ct0;
+	//testLoop(ct0);
 
 	std::cout << "[test1] name constructor" << std::endl;
-	ClapTrap ct1(ct0);
-	testLoop(ct1);
+	ClapTrap ct1("name_ct1");
+	//testLoop(ct1);
 
-	std::cout << "[test2] name constructor" << std::endl;
-	ClapTrap ct2 = ct0;
+	std::cout << "[test2] Copy constructor" << std::endl;
+	ClapTrap ct2(ct1);
 	testLoop(ct2);
 
-	std::cout << "[test3] name constructor" << std::endl;
-	ClapTrap ct3 = ClapTrap("name_ct3");
+	std::cout << "[test3] Copy constructor" << std::endl;
+	ClapTrap &ct3a = ct0;
+	ClapTrap ct3 = ct3a;
 	testLoop(ct3);
 
-	std::cout << "[test4] name constructor" << std::endl;
-	ClapTrap *ct4 = new ClapTrap("name_ct4");
-	testLoop(*ct4);
-	delete ct4;
+	std::cout << "[test4] Copy assignment & name constructor" << std::endl;
+	ClapTrap ct4 = ClapTrap("name_ct4");
+	testLoop(ct4);
+
+	std::cout << "[test5] new" << std::endl;
+	ClapTrap *ct5 = new ClapTrap("name_ct5");
+	testLoop(*ct5);
+	delete ct5;
 	return (0);
 }
